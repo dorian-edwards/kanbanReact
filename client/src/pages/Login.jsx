58d5de661bb111210d93db30b877025aac4af1a8
Dorian Edwards <de.pharmd2018@gmail.com>
2022-09-11 12:45:07 -0400

    finish up authcontext and protected routes

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -9,6 +9,8 @@ export default function Login() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
+  const value = useAuth()
+  console.log(value)
   const { login } = useAuth()
 
   const handleEmail = (e) => {
