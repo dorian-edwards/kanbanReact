53cf230ba41d36a6a01fa6e7f5d8b65797963ded
Dorian Edwards <de.pharmd2018@gmail.com>
2022-09-23 21:23:27 -0400

    add disabled styling to buttons

--- client-ts/src/components/Button.tsx
+++ client-ts/src/components/Button.tsx
@@ -1,10 +1,15 @@
 export interface ButtonProps {
   styling: string
   text: string
+  disabled: boolean
   type?: string
 }
 
 export default function Button(props: ButtonProps) {
-  const { styling, text } = props
-  return <button className={`btn ${styling}`}>{text}</button>
+  const { styling, text, disabled } = props
+  return (
+    <button className={`btn ${styling}`} disabled={disabled}>
+      {text}
+    </button>
+  )
 }
