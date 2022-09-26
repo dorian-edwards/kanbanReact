bd2a8dff5d1fc5aecc14f9ace7e963e4c1d49c4d
Dorian Edwards <de.pharmd2018@gmail.com>
2022-09-26 16:26:05 -0400

    add dynamic inputs to newBoardForm component

--- client-ts/src/components/NewBoardForm.tsx
+++ client-ts/src/components/NewBoardForm.tsx
@@ -3,8 +3,23 @@ import InputField from './inputs/InputField'
 import Button from './Button'
 
 export default function NewBoardForm() {
+  const [columnInputs, setColumnInputs] = useState<string[]>([])
+
+  const handleColumnInputChange = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    index: number
+  ): void => {
+    const data = [...columnInputs]
+    data[index] = e.target.value
+    setColumnInputs(data)
+  }
+
+  const createNewColumnInput = (): void => {
+    setColumnInputs([...columnInputs, ''])
+  }
+
   return (
-    <div className='new-board-form bg-white w-full max-w-[343px] h-[413px] mx-auto relative top-[279px]'>
+    <div className='new-board-form bg-white w-full max-w-[343px] min-h-[413px] mx-auto relative top-[279px]'>
       <form>
         <h2 className='heading-l mb-6 text-black'>Add New Board</h2>
         <InputField
@@ -14,11 +29,21 @@ export default function NewBoardForm() {
           onChange={() => console.log('hey')}
         />
         <h2 className='body-m text-med-gray mb-2'>Columns</h2>
+        {columnInputs.map((column: string, index: number) => {
+          return (
+            <InputField
+              key={index}
+              placeholder='e.g Done'
+              value={columnInputs[index]}
+              onChange={(e) => handleColumnInputChange(e, index)}
+            />
+          )
+        })}
         <Button
           styling='secondary mb-6'
           text='+ Add New Column'
           disabled={false}
-          onClick={(e) => console.log(e)}
+          onClick={createNewColumnInput}
           type='button'
         />
         <Button
