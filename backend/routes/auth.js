f24217c5823a8bd4b6f142eb71cd701ccc14d087
Dorian Edwards <de.pharmd2018@gmail.com>
2022-10-08 21:56:39 -0400

    sub in router export utility

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,8 +1,7 @@
-const express = require('express')
+const router = require('../utils/router')
 const passport = require('passport')
 const authController = require('../controllers/auth')
 const validation = require('../middleware/validation')
-const router = express.Router()
 
 router.get('/user', authController.getUser)
 router.post('/login', passport.authenticate('local'), authController.login)
